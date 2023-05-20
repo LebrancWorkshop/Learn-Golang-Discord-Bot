f0b71cd3147317f733050059f1bc75af75dfb381
lebrancconvas <lebrancconvas@gmail.com>
2023-05-20 21:12:51 +0700

    ✨ feat: create module

---
+++ modules/server/module.go
@@ -0,0 +1,23 @@
+package server
+
+import "github.com/bwmarrin/discordgo"
+
+type IModule interface {
+	GetCommandHandlers() map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
+}
+
+type module struct {
+	*discordServer
+	commandHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
+}
+
+func ModuleInit(s *discordServer) IModule {
+	return &module{
+		discordServer: s,
+		commandHandlers: make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)),
+	}
+}
+
+func (m *module) GetCommandHandlers() map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	return m.commandHandlers
+}
