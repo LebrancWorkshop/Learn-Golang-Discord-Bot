af3e3a863fc78bdf61c846940d27845bcf69639f
lebrancconvas <lebrancconvas@gmail.com>
2023-05-20 21:00:46 +0700

    ✨ feat: main function

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-
+	"github.com/LebrancWorkshop/Learn-Golang-Discord-Bot/config"
+	"github.com/LebrancWorkshop/Learn-Golang-Discord-Bot/modules/server"
 )
 
 func main() {
+	cfg := config.NewConfig()
 
+	server.NewDiscordServer(cfg).Start() 
 }
